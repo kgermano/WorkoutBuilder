857cea3a15fa1ea6156988aff2a7ba24c3362ce5
Katie Germano <kt.germano@gmail.com>
2018-01-16 13:05:48 -0600

    initial commit

---
+++ workout-builder/src/main/java/org/launchcode/workoutbuilder/WorkoutBuilderApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.workoutbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutBuilderApplication.class, args);
+	}
+}
